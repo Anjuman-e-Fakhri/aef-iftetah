971c97743c43410c91787f014c1a3ea7c92d327b
AeF-Mohsen <mohseny@mississaugajamaat.com>
2018-03-10 14:20:55 -0500

    added assigned and coheads

--- tasks/api/serializers.py
+++ tasks/api/serializers.py
@@ -2,20 +2,29 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from tasks.models import Task, Committee
 
+
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     committee = serializers.SlugRelatedField(
-            queryset=Committee.objects.all(), 
-            slug_field="name"
+        queryset=Committee.objects.all(),
+        slug_field="name"
+    )
+
+    assigned = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field="username",
+        allow_null=True
     )
 
     class Meta:
         model = Task
-        fields = ('url', 'id', 'committee', 'description', 'status', 'due_date')
+        fields = ('url', 'id', 'committee', 'description', 'status', 'due_date', 'assigned')
+
 
 class CommitteeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Committee
-        fields = ('url', 'id', 'name', 'head', 'members')
+        fields = ('url', 'id', 'name', 'heads', 'members')
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
--- tasks/models.py
+++ tasks/models.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 class Committee(models.Model):
-	name = models.CharField(max_length=30)
-	head = models.ForeignKey(User, on_delete="CASCADE", related_name="head")
-	members = models.ManyToManyField(User, related_name="members")
+    name = models.CharField(max_length=30)
+    heads = models.ManyToManyField(User, related_name="heads", blank=True)
+    members = models.ManyToManyField(User, related_name="members", blank=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
@@ -17,13 +18,11 @@ class Task(models.Model):
         ('In Progress', 'In Progress'),
         ('Completed', 'Completed'),
     )
-
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
     description = models.CharField(max_length=250)
     due_date = models.DateField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
     committee = models.ForeignKey(Committee, on_delete="CASCADE")
+    assigned = models.ForeignKey(User, on_delete="SET_NULL", null=True, blank=True)
 
     class Meta:
         ordering = ['committee', 'due_date', 'status']
